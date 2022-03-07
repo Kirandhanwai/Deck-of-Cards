7c80b78319d04ba9c11e10c5d78f9058306fd6c7
Kirandhanwai <kirandhanwai123@gmail.com>
2022-03-07 08:59:10 +0530

    Added : Deck of card created

---
+++ src/com/blz/java/DeckOfCard.java
@@ -0,0 +1,24 @@
+package com.blz.java;
+
+public class DeckOfCard {
+	String[] deck = new String[52];
+    String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+    String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    
+    public void alignCards() {
+    	int deckIndex = 0;
+    	for (int i = 0; i < suit.length; i++) {
+    	    for (int j = 0; j < rank.length; j++) {
+    		deck[deckIndex] = rank[j] + " of " + suit[i];
+    		deckIndex++;
+    	    }
+    	}
+    }	
+    	    
+    
+    	    public static void main(String[] args) {
+    	System.out.println("Welcome to Deck Of Card Java Project");
+    }
+}
+
+
